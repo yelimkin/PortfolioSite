b5d64ad9519c43248b6380927685bb219e2c6738
yelim <yelim785@gmail.com>
2022-12-13 15:38:03 +0900

    작성자 필드 추가

--- blog/models.py
+++ blog/models.py
@@ -1,17 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 import os
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=30)
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'[{self.pk} -- {self.title}]'
+        return f'[{self.pk} -- {self.title}] :: {self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from .models import Post
 # Create your views here.
 class PostList(ListView):
     model = Post
+    paginate_by = 3
     ordering = '-pk'
 
 class PostDetail(DetailView):
