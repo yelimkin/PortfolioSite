4b373b29612de7bcf017d9f182d97051feab7e34
yelim <yelim785@gmail.com>
2022-12-13 10:00:32 +0900

    미디어 파일 관리

--- icandoit/settings.py
+++ icandoit/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 import env_info # 베이스 폴더 밑에 있으므로 바로 import
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = env_info.BASE_DIR
@@ -121,3 +122,6 @@ STATICFILES_DIRS = (BASE_DIR / 'static',)
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
