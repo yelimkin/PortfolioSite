5c05f266a2b9a278af08af9dfb876ea58bcb0518
yelim <yelim785@gmail.com>
2022-12-14 10:14:44 +0900

    카테고리 목록 구성

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView
-from .models import Post
+from .models import Post, Category
 
 # Create your views here.
 class PostList(ListView):
@@ -7,6 +7,14 @@ class PostList(ListView):
     paginate_by = 3
     ordering = '-pk'
 
+    def get_context_data(self, **kwargs):
+        # get_context_data에서 기존에 제공했던 기능을 그대로 가져와 저장한다.
+        context = super(PostList, self).get_context_data()
+        # 모든 카테고리를 가져와서 'categories' 키에 연결해서 담는다.
+        context['categories'] = Category.objects.all()
+        # 카테고리가 지정되지 않은 포스트의 개수를 가져와서 'no_category_post_count'에 담는다.
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
+
 class PostDetail(DetailView):
     model = Post;
-    # 
