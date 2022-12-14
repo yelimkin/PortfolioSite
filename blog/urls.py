0a2e0fa3cc8ed6a65a6dc3a2c4e430edce87416e
yelim <yelim785@gmail.com>
2022-12-14 11:58:27 +0900

    카테고리 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ app_name = "blog"
 urlpatterns = [
     path('', views.PostList.as_view(), name='post_list'),
     path('<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
+    path('category/<str:slug>/', views.category_page),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
 from .models import Post, Category
+from django.shortcuts import render
 
 # Create your views here.
 class PostList(ListView):
@@ -24,4 +25,27 @@ class PostDetail(DetailView):
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
-        
+
+def category_page(request, slug):
+    if slug == 'no_category':
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+    else:
+        # 인자로 받은 slug와 동일한 slug를 갖는 카테고리를 
+        # 불러오는 쿼리셋을 만들어 categort 변수에 저장 
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
+
+    return render(
+        request, 
+        'blog/post_list.html',
+        {   
+            # 위에서 동일한 slug로 가져온 category만 가져오기
+            'post_list' : post_list,
+            # Post.objects.filter(category=category),
+            'categories' : Category.objects.all(),
+            'no_category_post_count' : Post.objects.filter(category=None).count(),
+            # 페이지 타이틀 옆에 카테고리 이름
+            'category' : category,
+        }
+    )
